0926749f80e22d485bda2e8b0f43d2f1e1be5759
Ricardo <GeekRicardozzZ@gmail.com>
2021-11-20 12:43:06 +0800

    使用fastapi重构/验证文件md5，重复的不重新写入

--- main.py
+++ main.py
@@ -10,18 +10,22 @@
 """
 
 # here put the import lib
-from flask import Flask, Response, request, render_template, jsonify
-from werkzeug.utils import secure_filename
+from typing import Optional
+from fastapi import FastAPI, Header, File, UploadFile, Response
+from fastapi.responses import FileResponse
+
+import uvicorn
+
 import os
 import hashlib
 import uuid
 
-app = Flask(__name__)
+app = FastAPI()
 
 
 # 设置图片保存文件夹
 UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), "images")
-app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+# app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
 # 设置允许上传的文件格式
 ALLOW_EXTENSIONS = ["png", "jpg", "jpeg"]
@@ -30,54 +34,48 @@ TOKEN_LIST = [
     "1fa81717-2bbc-4dca-a00f-887ebe7c2596"
 ]
 
-# 判断文件后缀是否在列表中
+
 def allowed_file(filename):
+    """
+    判断文件后缀是否在列表中
+    """
     return "." in filename and filename.rsplit(".", 1)[-1] in ALLOW_EXTENSIONS
 
 
-# 上传图片
-@app.route("/upload", methods=["POST", "GET"])
-def uploads():
-    if request.method == "POST":
-        # 验证token
-        token = request.headers.get("PToken")
-        if not token or token not in TOKEN_LIST:
-            return "-1"
+@app.post("/upload")
+async def uploads(
+    file: UploadFile = File(...),
+    PToken: Optional[str] = Header(None)
+):
+    # 验证token
+    if not PToken or PToken not in TOKEN_LIST:
+        return Response(status_code=401)
 
-        # 获取post过来的文件名称，从name=file参数中获取
-        file = request.files["file"]
-        if file and allowed_file(file.filename):
-            print(file.filename)
-            # secure_filename方法会去掉文件名中的中文
-            image_bytes = file.read()
-            md5 = hashlib.md5()
-            md5.update(image_bytes[:8096])
-            file_md5 = md5.hexdigest()
-            # file_md5 = str(uuid.uuid4())
-            file_name = file_md5 + "." + file.filename.rsplit(".", 1)[-1]
-            # 保存图片
-            with open(os.path.join(app.config["UPLOAD_FOLDER"], file_name), "wb") as f:
+    if file and allowed_file(file.filename):
+        image_bytes = await file.read()
+        md5 = hashlib.md5()
+        md5.update(image_bytes[:])
+        file_md5 = md5.hexdigest()
+        # file_md5 = str(uuid.uuid4())
+        file_name = file_md5 + "." + file.filename.rsplit(".", 1)[-1]
+        save_path = os.path.join(UPLOAD_FOLDER, file_name)
+        if not os.path.exists(save_path):
+            with open(save_path, "wb") as f:
                 f.write(image_bytes)
-            return file_name
-        else:
-            return "-1"
-    return render_template("index.html")
+        return Response(status_code=200, content=file_name)
+    else:
+        return Response(status_code=405, content="")
 
 
 # 查看图片
-@app.route("/<imageId>")
-def get_frame(imageId):
-    # 图片上传保存的路径
-    with open(
-        r"{}/{}".format(UPLOAD_FOLDER, imageId),
-        "rb",
-    ) as f:
-        print(imageId)
-        image = f.read()
-        filetype = "png" if "png" == imageId.rsplit(".")[-1] else "jpeg"
-        resp = Response(image, mimetype="image/" + filetype)
-        return resp
+@app.get("/{imageId}")
+async def get_frame(imageId, response=Response()):
+    if not os.path.exists(os.path.join(UPLOAD_FOLDER, imageId)):
+        response.status_code = 404
+        response.content = {"code": -1, "msg": "img not found."}
+        return response
+    return FileResponse("{}/{}".format(UPLOAD_FOLDER, imageId), media_type="image/jpg")
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3001)
+    uvicorn.run(app="main:app", host="0.0.0.0", port=3001, reload=True, debug=True)
