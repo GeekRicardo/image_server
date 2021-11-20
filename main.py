4a172ba1de76b7877ca323ec6bdda27ebbfc1591
Ricardo <GeekRicardozzZ@gmail.com>
2021-11-20 16:40:40 +0800

    [add]uuid/[update]flake8

--- main.py
+++ main.py
@@ -24,11 +24,14 @@ app = FastAPI()
 
 
 # 设置图片保存文件夹
-UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), "images")
+UPLOAD_FOLDER = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)),
+    "images"
+)
 # app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
 # 设置允许上传的文件格式
-ALLOW_EXTENSIONS = ["png", "jpg", "jpeg"]
+ALLOW_EXTENSIONS = ["png", "jpg", "jpeg", "exe"]
 
 TOKEN_LIST = [
     "1fa81717-2bbc-4dca-a00f-887ebe7c2596"
@@ -40,6 +43,11 @@ def allowed_file(filename):
     判断文件后缀是否在列表中
     """
     return "." in filename and filename.rsplit(".", 1)[-1] in ALLOW_EXTENSIONS
+
+
+@app.get("/uuid4")
+async def get_uuid():
+    return Response(content=str(uuid.uuid4()), status_code=200)
 
 
 @app.post("/upload")
@@ -74,8 +82,17 @@ async def get_frame(imageId, response=Response()):
         response.status_code = 404
         response.content = {"code": -1, "msg": "img not found."}
         return response
-    return FileResponse("{}/{}".format(UPLOAD_FOLDER, imageId), media_type="image/jpg")
+    return FileResponse(
+        "{}/{}".format(UPLOAD_FOLDER, imageId),
+        media_type="image/jpg"
+    )
 
 
 if __name__ == "__main__":
-    uvicorn.run(app="main:app", host="0.0.0.0", port=3001, reload=True, debug=True)
+    uvicorn.run(
+        app="main:app",
+        host="0.0.0.0",
+        port=3001,
+        reload=True,
+        debug=True
+    )
