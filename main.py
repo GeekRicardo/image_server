709440de9908c2de55357979a374d2795e80fc47
GeekRicardo <GeekRicardozzZ@gmail.com>
2023-03-13 23:47:00 +0800

    refactor(main): 重构使用数据库存文件信息
    BREAKING CHANGE:

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin python
 # -*- encoding: utf-8 -*-
 """
-@File    :   Untitled-1
+@File    :   main.py
 @Time    :   2021/11/04 18:13:24
 @Author  :   Ricardo
 @Version :   1.0
@@ -10,159 +10,143 @@
 """
 
 # here put the import lib
-from datetime import datetime
-from genericpath import isfile
-from typing import Optional, Union
-from fastapi import FastAPI, Header, File, UploadFile, Response
-from fastapi.responses import FileResponse, HTMLResponse
-
-import uvicorn
-
 import os
-import hashlib
 import uuid
+from datetime import datetime
 
-from model import session, FileBase, Msg
+import logging
+import uvicorn
+from fastapi import Depends, FastAPI, File, Request, Response, UploadFile
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from sqlalchemy import create_engine, Column, Integer, String, DateTime
+from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy.ext.declarative import declarative_base
+
+from rich.logging import RichHandler
+
+FORMAT = "%(message)s"
+logging.basicConfig(level="NOTSET", format=FORMAT, datefmt="[%X]", handlers=[RichHandler()])
+
+logger = logging.getLogger("main")
 
 app = FastAPI()
+app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
+templates = Jinja2Templates(directory="templates")
+
+Base = declarative_base()
 
 
-# 设置图片保存文件夹
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-UPLOAD_FOLDER = os.path.join(BASE_DIR, "images")
-# app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+UPLOAD_DIR = "uploads"
+engine = create_engine("sqlite:///file_records.db")
+SessionLocal = sessionmaker(bind=engine)
 
-# 设置允许上传的文件格式
-ALLOW_EXTENSIONS = ["png", "jpg", "jpeg", "exe", "pdf", "gif", "txt", "json"]
-ALLOW_TOKEN = False
-
-TOKEN_LIST = [
-    "1fa81717-2bbc-4dca-a00f-887ebe7c2596",
-    "af3d24f83456f002d5ab9918b6317ba0",
-]
+Base.metadata.create_all(bind=engine)
 
 
-def allowed_file(filename):
-    """
-    判断文件后缀是否在列表中
-    """
-    if not ALLOW_TOKEN:
-        return True
-    return "." in filename and filename.rsplit(".", 1)[-1] in ALLOW_EXTENSIONS
+class FileRecord(Base):
+    __tablename__ = "file_records"
+    id = Column(Integer, primary_key=True, index=True)
+    uuid = Column(String, unique=True, index=True)
+    filename = Column(String)
+    created_at = Column(DateTime, default=datetime.utcnow)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.on_event("startup")
+def startup():
+    # 创建数据库表
+    Base.metadata.create_all(bind=engine)
+
+
+@app.post("/upload/")
+async def upload(file: UploadFile = File(...), db: Session = Depends(get_db)):
+    # 保存上传的文件
+    uuid_filename = str(uuid.uuid4())
+    with open(f"{UPLOAD_DIR}/{uuid_filename}", "wb") as f:
+        f_bytes = file.file.read()
+        f.write(f_bytes)
+
+    # 保存上传记录到数据库
+    add_file = FileRecord(filename=file.filename, uuid=uuid_filename, created_at=datetime.utcnow())
+    db.add(add_file)
+    db.commit()
+    db.refresh(add_file)
+    return HTMLResponse(
+        f"<tr><td><a href='/{add_file.uuid}' target='_blank'>{add_file.uuid}"
+        + f"</a></td><td>{add_file.filename}</td><td>Now</td><td>{file.size}</td></tr>"
+    )
 
 
 @app.get("/")
-def index():
-    with open(os.path.join(BASE_DIR, "templates/index.html")) as f:
-        return HTMLResponse(f.read())
-    # return HTMLResponse(
-    #     """<form action="/upload" method="post" enctype="multipart/form-data">
-    #     <input type="file" name="file" id="file">
-    #     <input type="submit" value="上传">
-    # </form>"""
-    # )
+async def index(request: Request, db: Session = Depends(get_db)):
+    # 查询所有文件，按照上传时间排序
+    files = db.query(FileRecord).order_by(FileRecord.created_at.desc()).all()
 
-
-@app.get("/list")
-def get_file_list():
-    file_list = get_file_list_sortby_mtime()
-    return HTMLResponse(
-        """<ul>{}</ul>""".format(
-            "".join(
-                [
-                    f"<li><a href=\"/{it.md5}\">{it.filename} - <{it.ctime.strftime('%Y-%m-%d %H:%M:%S')}></a></li>"
-                    for it in file_list
-                ]
-            )
+    # 构建文件列表数据，包括文件名、原文件名、上传时间、文件大小等信息
+    file_list = []
+    for file in files:
+        file_path = os.path.join(UPLOAD_DIR, file.uuid)
+        file_size = os.path.getsize(file_path)
+        file_list.append(
+            {
+                "name": file.uuid,
+                "original_name": file.filename,
+                "created_at": file.created_at,
+                "size": file_size,
+            }
         )
-    )
+
+    # 渲染文件列表模板
+    return templates.TemplateResponse("index.html", {"request": request, "files": file_list})
 
 
-@app.get("/uuid4")
-async def get_uuid():
-    return Response(content=str(uuid.uuid4()), status_code=200)
+@app.get("/{filename}")
+async def download(filename: str, d: str = "", db: Session = Depends(get_db)):
+    # 根据UUID查询上传文件记录
+    upload_file = db.query(FileRecord).filter(FileRecord.uuid == filename).first()
 
+    # 如果上传文件记录不存在，则返回404
+    if not upload_file:
+        logger.error("文件不存在")
+        return Response(status_code=404, content="文件不存在")
 
-@app.post("/upload")
-async def uploads(file: UploadFile = File(...), html: Union[str,None] = Header(None)):
+    # 获取上传文件的本地路径
+    file_path = os.path.join(UPLOAD_DIR, upload_file.uuid)
 
-    if file and allowed_file(file.filename):
-        image_bytes = await file.read()
-        md5 = hashlib.md5()
-        md5.update(image_bytes[:])
-        file_md5 = md5.hexdigest()
-        filename = file.filename
-        save_path = os.path.join(UPLOAD_FOLDER, file_md5)
-        content = f"<a href='{file_md5}'>{filename}</a>"
-        if html and html == "0":
-            content = file_md5
-        if not os.path.exists(save_path):
-            with open(save_path, "wb") as f:
-                f.write(image_bytes)
-            new_file = FileBase(file_md5, filename, datetime.now())
-            try:
-                session.add(new_file)
-                session.commit()
-            except Exception as e:
-                print(e)
-                session.rollback()
-                return Response(status_code=500, content="database error")
-        else:
-            print("文件已存在.")
-        return Response(status_code=200, content=content)
-    else:
-        return Response(status_code=405, content="not allowed file type")
+    # 如果上传文件不存在，则返回404
+    if not os.path.exists(file_path):
+        logger.error("文件被移动或删除")
+        return Response(status_code=404, content="文件被移动或删除")
 
-@app.post("/static/upload")
-async def upload_static(file: UploadFile = File(...), html: Union[str,None] = Header(None)):
+    # 读取上传文件内容
+    with open(file_path, "rb") as f:
+        file_content = f.read()
 
-    image_bytes = await file.read()
-    filename = file.filename
-    save_path = os.path.join(UPLOAD_FOLDER, filename)
-    content = f"<a href='{filename}'>{filename}</a>"
-    if html and html == "0":
-        content = filename
-    if not os.path.exists(save_path):
-        with open(save_path, "wb") as f:
-            f.write(image_bytes)
-        new_file = FileBase(filename, filename, datetime.now())
-        try:
-            session.add(new_file)
-            session.commit()
-        except Exception as e:
-            print("static", e)
-            session.rollback()
-            return Response(status_code=500, content="database error")
-    else:
-        print("文件已存在.")
-    return Response(status_code=200, content=content)
-
-
-@app.get("/{imageId}")
-async def get_frame(imageId: str, d: str = "", response=Response()):
-    # if not imageId in os.listdir(UPLOAD_FOLDER):
-    #     response.status_code = 404
-    #     response.content = {"code": -1, "msg": "file not found."}
-    #     return response
-    try:
-        file_ = session.query(FileBase).filter(FileBase.md5 == imageId).one()
-    except Exception as e:
-        print(e)
-        response.status_code = 404
-        response.content = {"code": -1, "msg": "img not found."}
-        return response
-    media_type = "application/octet-stream"
-    filename = file_.filename
-    file_extension = filename.rsplit(".", 1)[-1].lower()
+    # 构造HTTP响应
+    file_extension = upload_file.filename.rsplit(".", 1)[-1].lower()
+    media_type = get_media_type(file_extension, d)
+    response = Response(content=file_content, media_type=media_type)
     if d == "1":
-        media_type = "application/octet-stream"
-        return FileResponse(
-        "{}/{}".format(UPLOAD_FOLDER, filename),
-        media_type=media_type,
-        filename=filename,
-    )
+        # 设置下载时的文件名
+        response.headers["Content-Disposition"] = f"attachment; filename={upload_file.filename}"
 
-    if "jpg" == file_extension or "jpeg" == file_extension:
+    return response
+
+
+def get_media_type(file_extension: str, direct_download: str = "") -> str:
+    media_type = "application/octet-stream"
+    if direct_download == "1":
+        media_type = "application/octet-stream"
+    elif file_extension in ["jpg", "jpeg"]:
         media_type = "image/jpeg"
     elif "png" == file_extension:
         media_type = "image/png"
@@ -174,50 +158,16 @@ async def get_frame(imageId: str, d: str = "", response=Response()):
         media_type = "application/json"
     elif "gif" == file_extension:
         media_type = "image/gif"
-    return FileResponse(
-        "{}/{}".format(UPLOAD_FOLDER, file_.md5),
-        media_type=media_type
-        )
-
-@app.get("/static/{filename}")
-def get_static_file(filename: str, d="", response=Response()):
-    if not os.path.exists(os.path.join(UPLOAD_FOLDER, filename)):
-        response.status_code = 404
-        response.content = {"code": -1, "msg": "file not found."}
-        return response
-    media_type = "application/octet-stream"
-    file_extension = filename.rsplit(".", 1)[-1].lower()
-    if d == "1":
-        media_type = "application/octet-stream"
-    elif "jpg" == file_extension or "jpeg" == file_extension:
-        media_type = "image/jpeg"
-    elif "png" == file_extension:
-        media_type = "image/png"
-    elif "txt" == file_extension:
-        media_type = "text/plain"
-    elif "pdf" == file_extension:
-        media_type = "application/pdf"
-    elif "json" == file_extension:
-        media_type = "application/json"
-    elif "gif" == file_extension:
-        media_type = "image/gif"
-    return FileResponse(
-        "{}/{}".format(UPLOAD_FOLDER, filename),
-        media_type=media_type,
-    )
-
-
-def get_file_list_sortby_mtime():
-    return sorted(session.query(FileBase), key=lambda x: x.ctime, reverse=True)
+    return media_type
 
 
 if __name__ == "__main__":
     uvicorn.run(
         app="main:app",
         host="0.0.0.0",
-        port=int(os.getenv("PORT", 3001)),
-        reload=bool(os.getenv("DEBUG", False)),
-        debug=bool(os.getenv("DEBUG", False)),
+        port=int(os.getenv("PORT", "3001")),
+        reload=bool(os.getenv("DEBUG", "false")),
+        # debug=bool(os.getenv("DEBUG", False)),
         # ssl_keyfile="/root/image_server/ssl/image.sshug.cn.key",
         # ssl_certfile="/root/image_server/ssl/image.sshug.cn_bundle.crt"
     )
