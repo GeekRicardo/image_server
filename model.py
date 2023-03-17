75666d7451692bd1a7aaf0a70301ab064755a89c
GeekRicardo <GeekRicardozzZ@gmail.com>
2023-03-17 21:52:49 +0800

    feat(model): 使Session单例限制数据库连接数

--- model.py
+++ model.py
@@ -1,52 +1,45 @@
-import os
-import pymysql
 from datetime import datetime
-from sqlalchemy import create_engine
+
+from sqlalchemy import create_engine, Column, Integer, String, DateTime
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, String, Integer, Text, DateTime
 
-pymysql.install_as_MySQLdb()
-engine = create_engine(os.environ["MYSQL_URI"], encoding="utf-8")
-Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-session = Session()
 Base = declarative_base()
+engine = create_engine("sqlite:///file_records.db")
+# SessionLocal = sessionmaker(bind=engine)
 
-
-class FileBase(Base):
-    __tablename__ = "file"
-
-    md5 = Column(String(255), primary_key=True)
-    filename = Column(String(255))
-    ctime = Column(DateTime)
-
-    def __init__(self, md5, filename, ctime=datetime.now()):
-        self.md5 = md5
-        self.filename = filename
-        self.ctime = ctime
-
-    def __str__(self):
-        return (
-            f"{self.filename} 【{self.md5}】<{self.ctime.strftime('%Y-%m-%d %H:%M:%S')}>"
-        )
-
-
-class Msg(Base):
-    __tablename__ = "msg"
-
-    id = Column(Integer, primary_key=True)
-    msg = Column(Text)
-    ctime = Column(DateTime)
-
-    def __init__(self, msg, id_=None, ctime=datetime.now()):
-        if id_:
-            self.id = id_
-        self.msg = msg
-        self.ctime = ctime
-
-    def __str__(self):
-        return f"{self.id} -【{self.msg}】<{self.ctime.strftime('%Y-%m-%d %H:%M:%S')}>"
-
-
-# 在数据库中生成表
 Base.metadata.create_all(bind=engine)
+
+
+class SessionLocal:
+    """单例限制数据库连接数"""
+
+    __instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if not cls.__instance:
+            cls.__instance = sessionmaker(bind=engine)()
+        return cls.__instance
+
+
+class FileRecord(Base):
+    __tablename__ = "file_records"
+    id = Column(Integer, primary_key=True, index=True)
+    uuid = Column(String, unique=True, index=True)
+    filename = Column(String)
+    created_at = Column(DateTime, default=datetime.utcnow)
+
+
+class MsgRecord(Base):
+    __tablename__ = "msg_records"
+    id = Column(Integer, primary_key=True, index=True)
+    message = Column(String)
+    created_at = Column(DateTime, default=datetime.utcnow)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()  # pylint: disable=E1101
